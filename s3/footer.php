2c35d37b53535b984465ae9e6dab86f07460618b
Isabella <isabellab.ferreira5@gmail.com>
2024-06-10 21:47:59 -0300

    feat: finalizado

--- s3/footer.php
+++ s3/footer.php
@@ -1,8 +1,56 @@
-<footer>
-
+<footer id="footer" class="bg-light-blue">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-4">
+                <a href="" class="logo"><img src="assets/img/logo.png" alt=""></a>
+                <div class="mt-5">
+                    <p>Experience personalized medical <br> care from the comfort of your home.</p>
+                </div>
+            </div>
+            <div class="col-3">
+                <h2>Support</h2>
+                <h3>Getting Started</h3>
+                <h3>Getting Started</h3>
+                <h3>Getting Started</h3>
+                <h3>Getting Started</h3>
+                <h3>Getting Started</h3>
+            </div>
+            <div class="col-3">
+                <h2>Support</h2>
+                <h3>Getting Started</h3>
+                <h3>Getting Started</h3>
+                <h3>Getting Started</h3>
+                <h3>Getting Started</h3>
+                <h3>Getting Started</h3>
+            </div>
+            <div class="col-2">
+                <h2>Support</h2>
+                <h3>Getting Started</h3>
+                <h3>Getting Started</h3>
+                <h3>Getting Started</h3>
+                <h3>Getting Started</h3>
+                <h3>Getting Started</h3>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-6">
+                <div class="d-flex gap-3">
+                    <a href="" class="font-20 text-secondary"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="" class="font-20 text-secondary"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="" class="font-20 text-secondary"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="" class="font-20 text-secondary"><i class="fa-brands fa-facebook"></i></a>
+                </div>
+            </div>
+            <div class="col-6 text-end text-light font-16">
+                <p>HealNet 2024 © All Rights Reserved</p>
+            </div>
+        </div>
+    </div>
 </footer>
 <script src="assets/js/app.js"></script>
-<?php //wp_footer(); ?>
+<?php //wp_footer(); 
+?>
 </body>
 
 </html>
--- s3/template-home.php
+++ s3/template-home.php
@@ -173,32 +173,244 @@ include("header.php");
         <div class="row margin-top-80">
           <div class="col-5">
             <div class="bloco-plataforma text-start mb-5">
-              <div class="d-flex gap-2 mb-3 ">
-                <div class="circle">
-                  <span>1</span>
+              <div class="row">
+                <div class="col-2">
+                  <div class="circle">
+                    <span>1</span>
+                  </div>
                 </div>
-                <h3>Create Your Profile</h3>
+                <div class="col-10">
+                  <h3>Create Your Profile</h3>
+                  <p>Sign up and fill in your medical history securely. Setting up your profile this way would ensure that you stay up-to-date with your medical processes.</p>
+                </div>
               </div>
-              <p>Sign up and fill in your medical history securely. Setting up your profile this way would ensure that you stay up-to-date with your medical processes.</p>
             </div>
             <div class="bloco-plataforma text-start mb-5">
-              <div class="d-flex gap-2 mb-3 ">
-                <div class="circle">
-                  <span>1</span>
+              <div class="row">
+                <div class="col-2">
+                  <div class="circle">
+                    <span>1</span>
+                  </div>
                 </div>
-                <h3>Create Your Profile</h3>
+                <div class="col-10">
+                  <h3>Create Your Profile</h3>
+                  <p>Sign up and fill in your medical history securely. Setting up your profile this way would ensure that you stay up-to-date with your medical processes.</p>
+                </div>
               </div>
-              <p>Sign up and fill in your medical history securely. Setting up your profile this way would ensure that you stay up-to-date with your medical processes.</p>
             </div>
             <div class="bloco-plataforma text-start mb-5">
-              <div class="d-flex gap-2 mb-3 ">
-                <div class="circle">
-                  <span>1</span>
+              <div class="row">
+                <div class="col-2">
+                  <div class="circle">
+                    <span>1</span>
+                  </div>
                 </div>
-                <h3>Create Your Profile</h3>
+                <div class="col-10">
+                  <h3>Create Your Profile</h3>
+                  <p>Sign up and fill in your medical history securely. Setting up your profile this way would ensure that you stay up-to-date with your medical processes.</p>
+                </div>
               </div>
-              <p>Sign up and fill in your medical history securely. Setting up your profile this way would ensure that you stay up-to-date with your medical processes.</p>
             </div>
+          </div>
+          <div class="col-7 d-flex align-items-center justify-content-end">
+            <div class="img-100">
+              <img src="assets/img/doctor-2.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="estatisticas">
+    <div class="container">
+      <div class="text-center">
+        <h2>Patient Testimonials:</h2>
+        <h3>Hear from Those We’ve Cared For</h3>
+        <p>Discover the difference we make through the voices of those we’ve served:</p>
+        <div class="depoimentos">
+          <div class="row">
+            <div class="col-6">
+              <div class="card-depoimento text-start">
+                <div class="row">
+                  <div class="col-3">
+                    <div class="img-100">
+                      <img src="assets/img/user.png" alt="">
+                    </div>
+                  </div>
+                  <div class="col-9">
+                    <div class="comentario">
+                      <p>“After my knee surgery, the convenience of online consultations made my recovery smoother than I could have imagined.”
+                      </p>
+                    </div>
+                    <h5>- Linda A.</h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-6">
+              <div class="card-depoimento text-start">
+                <div class="row">
+                  <div class="col-3">
+                    <div class="img-100">
+                      <img src="assets/img/user.png" alt="">
+                    </div>
+                  </div>
+                  <div class="col-9">
+                    <div class="comentario">
+                      <p>“After my knee surgery, the convenience of online consultations made my recovery smoother than I could have imagined.”
+                      </p>
+                    </div>
+                    <h5>- Linda A.</h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="estatistica">
+          <div class="row d-lg-flex d-none px-3 px-lg-0">
+            <div class="col-3 position-relative text-center">
+              <h2>
+                <span data-purecounter-start="0" data-purecounter-end="10000" data-purecounter-duration="1" class="purecounter">10000</span>
+                +
+              </h2>
+              <p>Successful Consultations</p>
+            </div>
+            <div class="col-3 position-relative text-center">
+              <h2>
+                <span data-purecounter-start="0" data-purecounter-end="2500" data-purecounter-duration="1" class="purecounter">2500</span>
+                +
+              </h2>
+              <p>Healthcare Professionals</p>
+            </div>
+            <div class="col-3 position-relative text-center">
+              <h2>
+                <span data-purecounter-start="0" data-purecounter-end="98" data-purecounter-duration="1" class="purecounter">98</span>
+                %
+              </h2>
+              <p>Patient Satisfaction Rate</p>
+            </div>
+            <div class="col-3 position-relative text-center">
+              <h2>
+                <span data-purecounter-start="0" data-purecounter-end="200" data-purecounter-duration="1" class="purecounter">200</span>
+                +
+              </h2>
+              <p>Top Specialists</p>
+            </div>
+          </div>
+        </div>
+        <div class="depoimentos">
+          <div class="row">
+            <div class="col-6">
+              <div class="card-depoimento text-start">
+                <div class="row">
+                  <div class="col-3">
+                    <div class="img-100">
+                      <img src="assets/img/user.png" alt="">
+                    </div>
+                  </div>
+                  <div class="col-9">
+                    <div class="comentario">
+                      <p>“After my knee surgery, the convenience of online consultations made my recovery smoother than I could have imagined.”
+                      </p>
+                    </div>
+                    <h5>- Linda A.</h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-6">
+              <div class="card-depoimento text-start">
+                <div class="row">
+                  <div class="col-3">
+                    <div class="img-100">
+                      <img src="assets/img/user.png" alt="">
+                    </div>
+                  </div>
+                  <div class="col-9">
+                    <div class="comentario">
+                      <p>“After my knee surgery, the convenience of online consultations made my recovery smoother than I could have imagined.”
+                      </p>
+                    </div>
+                    <h5>- Linda A.</h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="especialistas">
+    <div class="container">
+      <div class="text-center">
+        <h2>Masters of Medicine:</h2>
+        <h3>Meet our team of specialists</h3>
+        <p>Our team of specialists is at the forefront of medical innovation. Each specialist brings a unique blend of expertise, empathy, and experience to ensure that your health is in the best hands:</p>
+        <div class="especialistas">
+          <div class="card-especialistas py-4 px-5 mb-5">
+            <div class="row">
+              <div class="col-3">
+                <div class="img-100">
+                  <img src="assets/img/doctor-depoimentos.png" alt="">
+                </div>
+              </div>
+              <div class="col-9 text-start">
+                <h3>Dr. Sarah Johnson (Cardiologist)</h3>
+                <p>Heart health is Dr. Wong’s passion, and her approach to cardiology integrates cutting-edge technology with compassionate care. She’s a respected voice in the prevention of heart disease and a trusted partner to her patients on their journey to wellness.</p>
+                <div class="mt-3">
+                  <a href="" class="btn btn-white text-secondary">Book appointment <span class="ps-2"><i class="fa-solid fa-phone"></i></span></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card-especialistas py-4 px-5 mb-5">
+            <div class="row">
+              <div class="col-3">
+                <div class="img-100">
+                  <img src="assets/img/doctor-depoimentos.png" alt="">
+                </div>
+              </div>
+              <div class="col-9 text-start">
+                <h3>Dr. Sarah Johnson (Cardiologist)</h3>
+                <p>Heart health is Dr. Wong’s passion, and her approach to cardiology integrates cutting-edge technology with compassionate care. She’s a respected voice in the prevention of heart disease and a trusted partner to her patients on their journey to wellness.</p>
+                <div class="mt-3">
+                  <a href="" class="btn btn-white text-secondary">Book appointment <span class="ps-2"><i class="fa-solid fa-phone"></i></span></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="contato">
+    <div class="container">
+      <div class="text-center">
+        <h2>Reach our <span class="destaque">Help Desk</span> for support</h2>
+        <p>Questions? Need assistance? Our dedicated support team is here to help you every step of the way:</p>
+        <div class="formulario row margin-top-60">
+          <div class="col-4">
+            <form action="">
+              <div class="">
+                <label for="exampleFormControlInput1" class="form-label"></label>
+                <input type="text" class="form-control " id="exampleFormControlInput1" placeholder=" Enter Your First Name">
+              </div>
+            </form>
+          </div>
+          <div class="col-4">
+            <form action="">
+              <div class="">
+                <label for="exampleFormControlInput1" class="form-label"></label>
+                <input type="email" class="form-control " id="exampleFormControlInput1" placeholder="Enter Your Email Address">
+              </div>
+            </form>
+          </div>
+          <div class="col-4">
+            <form action="">
+              <button type="submit" class="btn btn-secondary text-white py-2 w-75">Contact us <span class="ps-2 text-white font-20"><i class="fa-solid fa-circle-arrow-right"></i></span></button>
+            </form>
           </div>
         </div>
       </div>
